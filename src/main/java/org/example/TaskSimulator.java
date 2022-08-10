1d5ae9723caa91dd3c15b57318e549f060999543
Yazan Kittaneh <yazan.kittane@progressoft.com>
2022-08-10 19:29:31 +0200

    small tweaks

--- src/main/java/org/example/TaskSimulator.java
+++ src/main/java/org/example/TaskSimulator.java
@@ -8,16 +8,24 @@ public class TaskSimulator {
 
     private static final Logger logger = Logger.getLogger(TaskSimulator.class.getName());
 
+    private static final Long RUN_TIME_IN_MINUTES = 60L;
+
     public static void main(String[] str) {
         logger.info("TaskSimulator called...");
         LocalTime startTime = LocalTime.now();
 
         logger.info("Start time = " + startTime.toString());
 
-        int sum = 0;
+        long sum = 0;
+        Random random = new Random();
 
-        while (!LocalTime.now().minusMinutes(15).isAfter(startTime))
-            sum = new Random().nextInt(5);
+        while (LocalTime.now().minusMinutes(RUN_TIME_IN_MINUTES).isBefore(startTime)) {
+            if (sum > Long.MAX_VALUE - 100L) {
+                logger.info("sum value reached [{}], going to reset it to 0 :)");
+                sum = 0;
+            }
+            sum += random.nextInt(5);
+        }
 
         logger.info("Finish time = " + LocalTime.now().toString() + ", sum = " + sum);
 
