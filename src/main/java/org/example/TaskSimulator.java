b3e5b6e997389bf01b95d98c40412893d5064d26
Yazan Kittaneh <yazan.kittane@progressoft.com>
2022-10-18 11:43:45 +0200

    run time can be passed as an argument now

--- src/main/java/org/example/TaskSimulator.java
+++ src/main/java/org/example/TaskSimulator.java
@@ -8,7 +8,7 @@ public class TaskSimulator {
 
     private static final Logger logger = Logger.getLogger(TaskSimulator.class.getName());
 
-    private static final Long RUN_TIME_IN_MINUTES = 60L;
+    private static final Long DEFAULT_RUN_TIME_IN_MINUTES = 60L;
 
     public static void main(String[] str) {
         logger.info("TaskSimulator called...");
@@ -19,7 +19,16 @@ public class TaskSimulator {
         long sum = 0;
         Random random = new Random();
 
-        while (LocalTime.now().minusMinutes(RUN_TIME_IN_MINUTES).isBefore(startTime)) {
+        long runTime;
+        try {
+            runTime = Long.parseLong(str[0]);
+            logger.info("going to use provided run time [" + runTime + "]");
+        } catch (Exception ex) {
+            logger.info("could not parse argument, going to use default run time [" + DEFAULT_RUN_TIME_IN_MINUTES + "]");
+            runTime = DEFAULT_RUN_TIME_IN_MINUTES;
+        }
+
+        while (LocalTime.now().minusMinutes(runTime).isBefore(startTime)) {
             if (sum > Long.MAX_VALUE - 100L) {
                 logger.info("sum value reached [{}], going to reset it to 0 :)");
                 sum = 0;
@@ -30,6 +39,5 @@ public class TaskSimulator {
         logger.info("Finish time = " + LocalTime.now().toString() + ", sum = " + sum);
 
         logger.info("TaskSimulator finished.");
-
     }
 }
